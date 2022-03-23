e3fa52f440f83185e948be79d779a65c195b4bb7
Randi Mays <randimays1024@gmail.com>
2022-03-22 21:58:00 -0500

    adding maps to new repo

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/// <reference types="@types/google.maps" />
+
+import { CustomMap } from './CustomMap';
+import { Company } from './Company';
+import { User } from './User';
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
